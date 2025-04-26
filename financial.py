abacd33699c05e44796f9851fcbf76db1e0ec3b7
Selvameena2002 <selvameena893@gmail.com>
2025-04-26 10:08:35 +0530

    Update financial.py

--- financial.py
+++ financial.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import pandas as pd
-from sklearn.ensemble import RandomForestRegressor
 from sklearn.preprocessing import LabelEncoder
 from sklearn.metrics import mean_absolute_error, r2_score
 import matplotlib.pyplot as plt
